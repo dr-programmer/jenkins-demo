f67f19f289bc847f5597ba983382baaa583d6366
dr-programmer <drtopuzov@gmail.com>
2025-06-02 00:53:56 +0300

    Some changes for CI/CD check

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const http = require('http');
 const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end("Hello from Jenkins Pipeline!");
+  res.end("Hello from Jenkins Pipeline!!!");
 });
 server.listen(port, () => console.log("Server running at port " + port));
